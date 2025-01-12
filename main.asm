53c81bda736bdb7a58eaaf2eaf39b4be0dc3288f
Aliqyan-21 <aliqyanabid21@gmail.com>
2025-01-12 13:47:52 +0530

    feat: raylib with asm - 'opening a window'

--- main.asm
+++ main.asm
@@ -5,13 +5,32 @@ section '.text' executable
 public _start
 extrn printf
 extrn _exit
+extrn InitWindow
+extrn WindowShouldClose
+extrn CloseWindow
+extrn BeginDrawing
+extrn EndDrawing
 
 _start:
-  mov rdi, msg
-  call printf
-  mov rdi, 0
-  call _exit
+  mov rdi, 800
+  mov rsi, 600
+  mov rdx, title
+  call InitWindow
+
+  ; testing the rax agains rax until it is zero, if zero window close, otherwise again
+  .again:
+    call WindowShouldClose
+    test rax, rax
+    jnz .close
+    call BeginDrawing
+    call EndDrawing
+  jmp .again
+
+  .close:
+    call CloseWindow
+    mov rdi, 0
+    call _exit
 
 
-section '.data' writeable
-msg: db "Hello World", 10, 0
+section '.data' writable
+title: db "Hello Raylib from asm"
